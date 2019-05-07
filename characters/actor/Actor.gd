e6492ea0ec9ab6a3b5d23cb4a1adb9fb89dd71b3
Nolic0321 <nolic0321@gmail.com>
2019-05-07 14:09:27 -0500

    Finished up the Actor Notification.  Works for the most part, can probably tighten it up to be more centered but it's something.  Almost closes out #24

--- characters/actor/Actor.gd
+++ characters/actor/Actor.gd
@@ -5,10 +5,18 @@ extends CharacterStats
 # Inventory of the Actor
 var inventory : Inventory
 
+# Notification Packed Scene
+const notify : PackedScene = preload("res://characters/actor/notify/ActorNotify.tscn")
+
 # Inventory start size
 export var inventory_size : int
 
-func _ready():
+# Notification Text Color
+export var notif_text_color : Color = Color(1,1,1,1)
+
+export var notif_range : int	# X-Range that Notification Can Randomly Stop at
+
+func _ready() -> void:
 	# Connect HealthBar update with health change
 	connect("health_changed",self,"update_health_bar")
 	connect("isdying", self, "_is_dying")
@@ -24,26 +32,38 @@ func _ready():
 	
 # Response to inventory's signal 'item_added'.  We will
 # simply do an actor notification
-func item_added_to_inventory(name : String):
-	$Notification.text = "+1 " + name
-	$AnimationPlayer.play("notify")
+func item_added_to_inventory(name : String) -> void:
+	rpc("notify","+1 " + name)
 	pass
 # Response to inventory's signal 'item_removed'.  We will
 # simply do an actor notification
-func item_removed_from_inventory(name : String):
-	$Notification.text = "-1 " + name
-	$AnimationPlayer.play("notify")
+func item_removed_from_inventory(name : String) -> void:
+	rpc("notify","-1 " + name)
 	pass
+
+remotesync func notify(text : String) -> void:
+	# Create new notify instance
+	var new_notif : EntityNotify = notify.instance()
+	# Calculate end point
+	var new_end : Vector2 = ($NotificationStop as Position2D).position
+	new_end.x += rand_range(-notif_range,notif_range)
+	# Set ActorNotify values
+	new_notif.end_pos =  new_end
+	new_notif.notification_text = text
+	new_notif.text_color = notif_text_color
+	new_notif.name = "Notification " + str($Notifications.get_child_count())
+	add_child(new_notif)
+	new_notif.notify()	# Run Notification
 
 # Update the health bar to reflect current CharacterStat health
 # Should only be a response from the "health_changed" signal sent by
 # CharacterStats
-func update_health_bar():
+func update_health_bar() -> void:
 	rpc("_update_health_bar")
 	
-remotesync func _update_health_bar():
+remotesync func _update_health_bar() -> void:
 	($HealthBar as TextureProgress).value = health
 
-puppet func _is_dying():
+puppet func _is_dying() -> void:
 	print_debug("ACTOR: %s Is Dying" % [name])
 	pass
